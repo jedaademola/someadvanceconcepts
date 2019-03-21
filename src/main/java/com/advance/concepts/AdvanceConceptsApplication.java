27a743af2fa45389388807c2048a252e7faa8d96
larogundade <lukman.arogundade@gmail.com>
2019-03-21 15:53:44 -0400

    Initial commit

---
+++ src/main/java/com/advance/concepts/AdvanceConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.advance.concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceConceptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvanceConceptsApplication.class, args);
+    }
+
+}
