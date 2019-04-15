f3e9459cca03c149a054a2f92578353d6ae99c8c
larogundade <lukman.arogundade@gmail.com>
2019-04-15 10:58:12 -0400

    Spring Batch processing

---
+++ src/main/java/com/advance/concepts/util/TestString.java
@@ -0,0 +1,16 @@
+package com.advance.concepts.util;
+
+public class TestString {
+
+    public static void main(String args[]) {
+        //
+        String isbn = "978-1-86197-876-9";
+        //
+        System.out.println(isbn.substring(0, 3));
+        System.out.println(isbn.substring(4, 5));
+        System.out.println(isbn.substring(6, 11));
+        System.out.println(isbn.substring(12, 15));
+        System.out.println(isbn.substring(16));
+        //
+    }
+}
